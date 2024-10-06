2fbf881e5e8bc26ce5658b69acaf9483082c80c0
PrPleGoo <felix.leeuwen@gmail.com>
2024-10-06 20:10:33 +0200

    fix check

--- regions/src/main/java/prplegoo/regions/api/MagicStringChecker.java
+++ regions/src/main/java/prplegoo/regions/api/MagicStringChecker.java
@@ -1,7 +1,14 @@
 package prplegoo.regions.api;
 
+import snake2d.LOG;
+
 public class MagicStringChecker {
     public static boolean isResourceProductionBooster(String key) {
-        return key.contains("RESOURCE_");
+        boolean result = key.startsWith("WORLD_RESOURCE_PRODUCTION_");
+        if(result) {
+            LOG.ln("isResourceProductionBooster, " + key);
+        }
+
+        return result;
     }
 }

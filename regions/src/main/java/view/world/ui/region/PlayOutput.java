31940a452f696c6842c848b23ae3d8a54d86b0f6
PrPleGoo <felix.leeuwen@gmail.com>
2025-01-16 18:43:08 +0100

    update file

--- regions/src/main/java/view/world/ui/region/PlayOutput.java
+++ regions/src/main/java/view/world/ui/region/PlayOutput.java
@@ -1,7 +1,6 @@
 package view.world.ui.region;
 
 import init.sprite.UI.UI;
-import prplegoo.regions.api.RDSlavery;
 import snake2d.SPRITE_RENDERER;
 import snake2d.util.gui.GUI_BOX;
 import snake2d.util.gui.GuiSection;
@@ -18,25 +17,22 @@ import world.map.regions.Region;
 import world.region.RD;
 import world.region.RDOutput.RDResource;
 
-final class PlayOutput extends GuiSection {
+final class PlayOutput extends GuiSection{
 
 
-    private final RENDEROBJ[] butts = new RENDEROBJ[RD.OUTPUT().all.size() + RD.SLAVERY().size()];
+
+    private final RENDEROBJ[] butts = new RENDEROBJ[RD.OUTPUT().all.size()];
     private final GETTER_IMP<Region> g;
-    private final ArrayList<RENDEROBJ> activeButts = new ArrayList<RENDEROBJ>(RD.OUTPUT().all.size() + RD.SLAVERY().size());
+    private final ArrayList<RENDEROBJ> activeButts = new ArrayList<RENDEROBJ>(RD.OUTPUT().all.size());
     private final int width;
     public static final int height = 30;
     private final int amX;
 
     public PlayOutput(GETTER_IMP<Region> g, int width) {
-        this.width = (width - 24) / 5;
+        this.width = (width-24)/5;
         this.g = g;
         for (int i = 0; i < RD.OUTPUT().all.size(); i++) {
             butts[i] = new ResButt(RD.OUTPUT().all.get(i));
-        }
-
-        for (int i = 0; i < RD.RACES().all.size(); i++) {
-            butts[i + RD.OUTPUT().all.size()] = new RaceButt(RD.SLAVERY().get(i));
         }
 
         amX = 5;
@@ -46,11 +42,11 @@ final class PlayOutput extends GuiSection {
 
             @Override
             public int nrOFEntries() {
-                return (int) Math.ceil(activeButts.size() / (double) amX);
+                return (int) Math.ceil(activeButts.size()/(double)amX);
             }
         };
 
-        builder.column(null, amX * this.width, new GRowBuilder() {
+        builder.column(null, amX*this.width, new GRowBuilder() {
 
             @Override
             public RENDEROBJ build(GETTER<Integer> ier) {
@@ -58,7 +54,7 @@ final class PlayOutput extends GuiSection {
             }
         });
 
-        add(builder.createHeight((height) * 2, false));
+        add(builder.createHeight((height)*2, false));
 
 
     }
@@ -68,28 +64,18 @@ final class PlayOutput extends GuiSection {
         activeButts.clearSloppy();
         for (int i = 0; i < RD.OUTPUT().all.size(); i++) {
             RDResource b = RD.OUTPUT().all.get(i);
-
-            if (b.boost.get(g.get()) != 0) {
+            if (b.boost.get(g.get()) > 0) {
                 activeButts.add(butts[i]);
             }
         }
-
-        for (int i = 0; i < RD.RACES().all.size(); i++) {
-            RDSlavery.RDSlave b = RD.SLAVERY().get(i);
-
-            if (b.boost.get(g.get()) != 0) {
-                activeButts.add(butts[i + RD.OUTPUT().all.size()]);
-            }
-        }
-
         super.render(r, ds);
     }
 
-    private class Row extends GuiSection {
+    private class Row extends GuiSection{
 
         private final GETTER<Integer> ier;
 
-        Row(GETTER<Integer> ier) {
+        Row(GETTER<Integer> ier){
             this.ier = ier;
             body().setHeight(height);
         }
@@ -99,9 +85,9 @@ final class PlayOutput extends GuiSection {
             int x1 = body().x1();
             int y1 = body().y1();
             clear();
-            int s = ier.get() * amX;
-            for (int i = 0; i < amX && i + s < activeButts.size(); i++) {
-                addRightC(0, activeButts.get(i + s));
+            int s = ier.get()*amX;
+            for (int i = 0; i < amX && i+s < activeButts.size(); i++) {
+                addRightC(0, activeButts.get(i+s));
             }
             body().moveX1(x1);
             body().moveY1(y1);
@@ -110,12 +96,12 @@ final class PlayOutput extends GuiSection {
 
     }
 
-    private class ResButt extends ClickableAbs {
+    private class ResButt extends ClickableAbs{
 
         private final RDResource bu;
         private final GText tt = new GText(UI.FONT().S, 8);
 
-        ResButt(RDResource b) {
+        ResButt(RDResource b){
             body.setDim(width, height);
             this.bu = b;
         }
@@ -124,51 +110,25 @@ final class PlayOutput extends GuiSection {
         protected void render(SPRITE_RENDERER r, float ds, boolean isActive, boolean isSelected, boolean isHovered) {
 
             GCOLOR.UI().border().render(r, body);
-            GCOLOR.UI().bg(isActive, isSelected, isHovered).render(r, body, -1);
+            GCOLOR.UI().bg(isActive, isSelected, isHovered).render(r, body,-1);
 
-            bu.res.icon().renderC(r, body.x1() + 16, body.cY());
+            bu.res.icon().renderC(r, body.x1()+16, body.cY());
 
             tt.clear();
             GFORMAT.i(tt, (long) bu.boost.get(g.get()));
 
-            tt.renderC(r, body.x1() + 32, body.cY());
+            tt.renderC(r, body.x1()+32, body.cY());
+
+
+
         }
 
         @Override
         public void hoverInfoGet(GUI_BOX text) {
             bu.boost.hover(text, g.get(), true);
+
         }
 
     }
 
-    private class RaceButt extends ClickableAbs {
-
-        private final RDSlavery.RDSlave rdSlave;
-        private final GText tt = new GText(UI.FONT().S, 8);
-
-        RaceButt(RDSlavery.RDSlave rdSlave) {
-            body.setDim(width, height);
-            this.rdSlave = rdSlave;
-        }
-
-        @Override
-        protected void render(SPRITE_RENDERER r, float ds, boolean isActive, boolean isSelected, boolean isHovered) {
-
-            GCOLOR.UI().border().render(r, body);
-            GCOLOR.UI().bg(isActive, isSelected, isHovered).render(r, body, -1);
-
-            rdSlave.rdRace.race.appearance().icon.renderC(r, body.x1() + 16, body.cY());
-
-            tt.clear();
-            GFORMAT.i(tt, rdSlave.getDelivery(g.get(), 1));
-
-            tt.renderC(r, body.x1() + 32, body.cY());
-        }
-
-        @Override
-        public void hoverInfoGet(GUI_BOX text) {
-            rdSlave.boost.hover(text, g.get(), true);
-        }
-
-    }
 }

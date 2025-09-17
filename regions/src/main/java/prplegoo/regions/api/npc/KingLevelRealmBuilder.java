1ef2726bd2b26f7ef403e25f135cdd1bcbe0f946
Dinosaure <gweltaz27@hotmail.com>
2025-09-18 00:07:31 +0200

    Tabs -> Spaces

--- regions/src/main/java/prplegoo/regions/api/npc/KingLevelRealmBuilder.java
+++ regions/src/main/java/prplegoo/regions/api/npc/KingLevelRealmBuilder.java
@@ -16,93 +16,93 @@ import world.region.pop.RDRace;
 import world.region.pop.RDRaces;
 
 public class KingLevelRealmBuilder {
-	public void build(FactionNPC faction) {
-		RDRaces races = RD.RACES();
-		double[] genocide = new double[races.all.size()];
-		Noble noble = BOOSTABLES.NOBLE();
-		Royalty king = faction.king();
-		double proclivity = noble.AGRESSION.get(king.induvidual)
-				/ noble.TOLERANCE.get(king.induvidual)
-				/ noble.MERCY.get(king.induvidual);
-		
-		for (RDRace race : races.all) {
-			genocide[race.index()] = (race.race.index == king.induvidual.race().index)
-				? 0
-				: (1 - king.induvidual.race().pref().race(race.race)) * proclivity;
-		}
+    public void build(FactionNPC faction) {
+        RDRaces races = RD.RACES();
+        double[] genocide = new double[races.all.size()];
+        Noble noble = BOOSTABLES.NOBLE();
+        Royalty king = faction.king();
+        double proclivity = noble.AGRESSION.get(king.induvidual)
+                / noble.TOLERANCE.get(king.induvidual)
+                / noble.MERCY.get(king.induvidual);
+        
+        for (RDRace race : races.all) {
+            genocide[race.index()] = (race.race.index == king.induvidual.race().index)
+                ? 0
+                : (1 - king.induvidual.race().pref().race(race.race)) * proclivity;
+        }
 
-		for (Region region : faction.realm().all())
-			for (RDRace race : races.all) {
-				RDEdicts edicts = races.edicts;
-				edicts.massacre.toggled(race).set(region, genocide[race.index()] > 3.0 ? 1 : 0);
-				edicts.exile.toggled(race).set(region, 0);
-				edicts.sanction.toggled(race).set(region, 0);
-			}
+        for (Region region : faction.realm().all())
+            for (RDRace race : races.all) {
+                RDEdicts edicts = races.edicts;
+                edicts.massacre.toggled(race).set(region, genocide[race.index()] > 3.0 ? 1 : 0);
+                edicts.exile.toggled(race).set(region, 0);
+                edicts.sanction.toggled(race).set(region, 0);
+            }
 
-		// do genocide aggression, tolerance, mercy, rng on king name?
-		// don't genocide own species, ever
-		FactionGenetic original = new FactionGenetic(faction);
-		calculateMutatorFitness(original, faction);
+        // do genocide aggression, tolerance, mercy, rng on king name?
+        // don't genocide own species, ever
+        FactionGenetic original = new FactionGenetic(faction);
+        calculateMutatorFitness(original, faction);
 
-		boolean alertMode = original.anyFitnessExceedsDeficit(faction);
-		if (alertMode)
-			for (Region region : faction.realm().all())
-				for (RDBuilding building : RD.BUILDINGS().all) {
-					int buildingLevel = building.level.get(region);
-					if (buildingLevel > 0) building.level.set(region, buildingLevel - 1);
-				}
+        boolean alertMode = original.anyFitnessExceedsDeficit(faction);
+        if (alertMode)
+            for (Region region : faction.realm().all())
+                for (RDBuilding building : RD.BUILDINGS().all) {
+                    int buildingLevel = building.level.get(region);
+                    if (buildingLevel > 0) building.level.set(region, buildingLevel - 1);
+                }
 
-		KingLevels kingLevelsInstance = KingLevels.getInstance();
+        KingLevels kingLevelsInstance = KingLevels.getInstance();
 
-		for (int i = 0; i < GeneticVariables.mutationAttemptsPerTick; i++) {
-			MutationStrategy strategy = (alertMode
-					? alertStrategies
-					: strategies).Pick();
-			
-			FactionGeneticMutator mutator = new FactionGeneticMutator(faction, strategy);
-			if (!mutator.tryMutate()) continue;
-			calculateMutatorFitness(mutator, faction, kingLevelsInstance);
+        for (int i = 0; i < GeneticVariables.mutationAttemptsPerTick; i++) {
+            MutationStrategy strategy = (alertMode
+                    ? alertStrategies
+                    : strategies).Pick();
+            
+            FactionGeneticMutator mutator = new FactionGeneticMutator(faction, strategy);
+            if (!mutator.tryMutate()) continue;
+            calculateMutatorFitness(mutator, faction, kingLevelsInstance);
 
-			original = new FactionGeneticMutator(faction, strategy);
-			calculateMutatorFitness(original, faction, kingLevelsInstance);
-			if (!original.shouldAdopt(faction, mutator)) original.commit();
-		}
+            original = new FactionGeneticMutator(faction, strategy);
+            calculateMutatorFitness(original, faction, kingLevelsInstance);
+            if (!original.shouldAdopt(faction, mutator)) original.commit();
+        }
 
-		kingLevelsInstance.resetDailyProductionRateCache(faction);
-	}
+        kingLevelsInstance.resetDailyProductionRateCache(faction);
+    }
 
-	private void calculateMutatorFitness(FactionGenetic mutant, FactionNPC faction, KingLevels KingLevelsInstance) {
-		KingLevelsInstance.resetDailyProductionRateCache(faction);
-		calculateMutatorFitness(mutant, faction);
-	}
+    private void calculateMutatorFitness(FactionGenetic mutant, FactionNPC faction, KingLevels KingLevelsInstance) {
+        KingLevelsInstance.resetDailyProductionRateCache(faction);
+        calculateMutatorFitness(mutant, faction);
+    }
 
-	private void calculateMutatorFitness(FactionGenetic mutant, FactionNPC faction) {
-		mutant.loadFitness(faction).calculateFitness(faction);
-	}
+    private void calculateMutatorFitness(FactionGenetic mutant, FactionNPC faction) {
+        mutant.loadFitness(faction).calculateFitness(faction);
+    }
 
-	public KingLevelRealmBuilder() {
-		strategies.Add(1, ReduceWorkforceDeficitMutationStrategy);
-		strategies.Add(1, PopulationGrowthMutationStrategy);
-		strategies.Add(1, HealthMutationStrategy);
-		strategies.Add(1, LoyaltyMutationStrategy);
-		strategies.Add(1, ReduceDeficitMutationStrategy);
-		strategies.Add(3, PrimarySectorStrategy);
-		strategies.Add(4, ReduceStorageMutationStrategy);
+    public KingLevelRealmBuilder() {
+        strategies.Add(1, ReduceWorkforceDeficitMutationStrategy);
+        strategies.Add(1, PopulationGrowthMutationStrategy);
+        strategies.Add(1, HealthMutationStrategy);
+        strategies.Add(1, LoyaltyMutationStrategy);
+        strategies.Add(1, ReduceDeficitMutationStrategy);
+        strategies.Add(3, PrimarySectorStrategy);
+        strategies.Add(4, ReduceStorageMutationStrategy);
 
-		alertStrategies.Add(1, PopulationGrowthMutationStrategy);
-		alertStrategies.Add(4, HealthMutationStrategy);
-		alertStrategies.Add(4, LoyaltyMutationStrategy);
-		alertStrategies.Add(2, ReduceWorkforceDeficitMutationStrategy);
-	}
+        alertStrategies.Add(1, PopulationGrowthMutationStrategy);
+        alertStrategies.Add(4, HealthMutationStrategy);
+        alertStrategies.Add(4, LoyaltyMutationStrategy);
+        alertStrategies.Add(2, ReduceWorkforceDeficitMutationStrategy);
+    }
 
-	private static final WeightedBag<MutationStrategy> strategies = new WeightedBag<>();
-	private static final WeightedBag<MutationStrategy> alertStrategies = new WeightedBag<>();
-	private static final ReduceWorkforceDeficitMutationStrategy ReduceWorkforceDeficitMutationStrategy = new ReduceWorkforceDeficitMutationStrategy();
-	private static final PopulationGrowthMutationStrategy PopulationGrowthMutationStrategy = new PopulationGrowthMutationStrategy();
-	private static final HealthMutationStrategy HealthMutationStrategy = new HealthMutationStrategy();
-	private static final LoyaltyMutationStrategy LoyaltyMutationStrategy = new LoyaltyMutationStrategy();
-	private static final ReduceDeficitMutationStrategy ReduceDeficitMutationStrategy = new ReduceDeficitMutationStrategy();
-	private static final PrimarySectorStrategy PrimarySectorStrategy = new PrimarySectorStrategy();
-	private static final ReduceStorageMutationStrategy ReduceStorageMutationStrategy = new ReduceStorageMutationStrategy();
+    private static final WeightedBag<MutationStrategy> strategies = new WeightedBag<>();
+    private static final WeightedBag<MutationStrategy> alertStrategies = new WeightedBag<>();
+    private static final ReduceWorkforceDeficitMutationStrategy ReduceWorkforceDeficitMutationStrategy = new ReduceWorkforceDeficitMutationStrategy();
+    private static final PopulationGrowthMutationStrategy PopulationGrowthMutationStrategy = new PopulationGrowthMutationStrategy();
+    private static final HealthMutationStrategy HealthMutationStrategy = new HealthMutationStrategy();
+    private static final LoyaltyMutationStrategy LoyaltyMutationStrategy = new LoyaltyMutationStrategy();
+    private static final ReduceDeficitMutationStrategy ReduceDeficitMutationStrategy = new ReduceDeficitMutationStrategy();
+    private static final PrimarySectorStrategy PrimarySectorStrategy = new PrimarySectorStrategy();
+    private static final ReduceStorageMutationStrategy ReduceStorageMutationStrategy = new ReduceStorageMutationStrategy();
 }
 

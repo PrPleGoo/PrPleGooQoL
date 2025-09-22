72dbdafc361b4e5434af3e614f6a21f68a4b5598
Dinosaure <gweltaz27@hotmail.com>
2025-09-22 12:48:36 +0200

    Better naming scheme: (a, b) -> a | b becomes (didMutationOccur, mutateRegionResult) -> didMutationOccur | mutateRegionResult

--- regions/src/main/java/prplegoo/regions/api/npc/buildinglogic/strategy/LoopingMutationStrategy.java
+++ regions/src/main/java/prplegoo/regions/api/npc/buildinglogic/strategy/LoopingMutationStrategy.java
@@ -1,6 +1,5 @@
 package prplegoo.regions.api.npc.buildinglogic.strategy;
 
-import prplegoo.regions.api.npc.buildinglogic.BuildingGenetic;
 import prplegoo.regions.api.npc.buildinglogic.FactionGenetic;
 import prplegoo.regions.api.npc.buildinglogic.RegionGenetic;
 import world.WORLD;
@@ -12,7 +11,7 @@ public abstract class LoopingMutationStrategy extends MutationStrategy {
     public boolean tryMutate(FactionGenetic factionGenetic) {
         return Arrays.stream(factionGenetic.getRegionGenetics())
                 .map(this::mutateRegion)
-                .reduce(false, (a, b) -> a | b); // didMutationOccur = false, |= mutateRegion
+                .reduce(false, (didMutationOccur, mutateRegionResult) -> didMutationOccur | mutateRegionResult);
     }
 
     public boolean mutateRegion(RegionGenetic regionGenetic) {
@@ -20,6 +19,6 @@ public abstract class LoopingMutationStrategy extends MutationStrategy {
 
         return Arrays.stream(regionGenetic.buildingGenetics)
                 .map(building -> tryMutateBuilding(building, region))
-                .reduce(false, (a, b) -> a | b); // didMutationOccur = false, |= tryMutateBuilding
+                .reduce(false, (didMutationOccur, tryMutateBuildingResult) -> didMutationOccur | tryMutateBuildingResult);
     }
 }
